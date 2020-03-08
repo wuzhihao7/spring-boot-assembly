1960c1daa3be40c931eb55d48fccb8b4a09a8bd4
house <wuzhihao7@outlook.com>
2020-03-08 16:16:06 +0800

    Initial commit

---
+++ src/main/java/com/technologysia/assembly/SpringBootAssemblyApplication.java
@@ -0,0 +1,13 @@
+package com.technologysia.assembly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAssemblyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAssemblyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/technologysia/assembly/SpringBootAssemblyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technologysia.assembly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAssemblyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
